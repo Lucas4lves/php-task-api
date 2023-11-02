128154fbac8f49df90ec48b4742d570e18a5c895
lucas4lves <zombnfl@gmail.com>
2023-11-01 22:53:03 -0300

    Adicionando respostas em JSON para a conexão com o banco de dados

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('./model/Response.php');
+
 $db_host = "server-mysql";
 $db_username = "root";
 $db_password = "mypassword";
@@ -8,9 +10,17 @@ $db_name = "mysql";
 try {
 	$connection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
 	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "MySQL Connection was succeeded! Hooray!";
+	$res = new Response();
+	$res->setHttpStatusCode(200);
+	$res->setSuccess(true);
+	$res->addMessage("MySQL connection succesfully established");
+	$res->send(); 
 }
 catch(PDOException $ex)
 {
-	echo "MySQL Connection failed with: ERROR->" . $ex->getMessage() . PHP_EOL;
+	$res = new Response();
+	$res->setHttpStatusCode(400);
+	$res->setSuccess(false);
+	$res->addMessage("MySQL Connection failed with: ERROR->" . $ex->getMessage() . PHP_EOL);
+	$res->send(); 
 }	
